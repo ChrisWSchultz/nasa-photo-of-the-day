bbc777b534dae737b4e191c82736aac35bde56cf
Christopher W Schultz <chris.william.schultz@gmail.com>
2020-03-06 15:13:25 -0700

    date picker fixed

--- src/APOD.js
+++ src/APOD.js
@@ -1,45 +1,23 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-import { BASE_URL, KEY, DATE_QUERY } from "./config";
+import { BASE_URL, DATE_QUERY } from "./config";
 
-import PictureCard, { SideButton, ControlsContainer } from "./pictureCard";
-
-const url = `${BASE_URL}${KEY}${DATE_QUERY}`;
-const days = buildDates(6);
-
-function buildDates(number) {
-    const list = [];
-    for(let i = 0; i < number; i++) {
-        const picture = new Date();
-        picture.setDate(picture.getDate() + (-i - 1));
-        list.push(`${url}${picture.toISOString().slice(0,10)}`);
-    }
-    return list;
-}
-
+import PictureCard, { DatePicker, FullScreen } from "./pictureCard";
 
 
 export function PictureOfTheDay() {
 
-    const [next, setNext] = useState(days[0]);
-    const [current, setCurrent] = useState(0);
     const [data, setData ] = useState(0);
+    const [link, setLink] = useState(BASE_URL);
 
-    function handleClick(num) {
-        setCurrent(current + num);
-        setNext(days[current]);
+    function getDate() {
 
-        if(current === days.length) {
-            setCurrent(0);
-        } else if(current < days.length) {
-            setCurrent(days.length - 1);
-        }
     }
 
     useEffect(() => {
         axios
-            .get(`${next}`)
+            .get(`${link}`)
             .then(response => {
                 console.log(response);
                 setData(response.data);
@@ -48,12 +26,12 @@ export function PictureOfTheDay() {
 
     useEffect(() => {
         axios
-            .get(`${next}`)
+            .get(`${link}`)
             .then(response => {
                 console.log(response);
                 setData(response.data);
             });
-    }, [next]);
+    }, [link]);
 
     useEffect( () => {
         if (data && data.media_type === 'image') {
@@ -67,12 +45,12 @@ export function PictureOfTheDay() {
         return (<p>Loading...</p>)
     }
     return (
-    <ControlsContainer>
-        <SideButton onClick={()=>{console.log('click'); handleClick(-1);}}>
-            Previous</SideButton>
+    <FullScreen>
+        <div>
+            <DatePicker onChange={(event) => {setLink(`${DATE_QUERY}${event.target.value}`)}}/>
+        </div>
+
         <PictureCard data={data} />
-        <SideButton onClick={()=>{console.log('click'); handleClick(1)}}>
-            Next</SideButton>
-    </ControlsContainer>
+    </FullScreen>
     );
 }
--- src/pictureCard.js
+++ src/pictureCard.js
@@ -1,26 +1,32 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+const today = new Date();
+
 const CardContainer = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
     align-items: center;
     width: 100%;
-    height: 100vh;
 `
 const CardTitle = styled.h1`
+    font-family: 'Abel', sans-serif;
+    font-size: 2.6rem;
     margin-top: 0;
-    margin-bottom: 40px;
+    margin-bottom: 20px;
 `
 const InfoContainer = styled.div`
     background-color: ${(props) => props.showCard ? 'white' : 'none'};
     border-radius: 20px 20px 0 0 ;
     padding: 20px 50px 0;
-    width: 600px;
+    width: 800px;
     color: ${(props) => props.showCard ? 'black' : 'white'};
 `
 const Paragraph = styled.div`
+    font-family: 'Raleway', sans-serif;
+    font-size: 1.2rem;
+    line-height: 1.4;
     padding-bottom: 20px;
     display: ${(props) => props.showCard ? 'border-box': 'none'};
 `
@@ -29,13 +35,26 @@ const Video = styled.div`
     justify-content: center;
     width: 100%;
 `
-export const SideButton = styled.button`
-    width: 60px;
-    color: white;
-`
-export const ControlsContainer = styled.div`
+export const FullScreen = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+`
+export const DatePicker = styled.input.attrs({
+    type: 'date',
+    class: 'date-picker',
+    value: `${today.toISOString().slice(0, 10)}`,
+    min: '2015-01-01',
+    max: `${today.toISOString().slice(0, 10)}`
+})`
+    font-family: 'Abel', sans-serif;
+    font-size: 1.4rem;
+    color: white;
+    border: none;
+    background-color: transparent;
 `
 
 
@@ -50,6 +69,7 @@ function PictureCard(props) {
                     <Paragraph showCard={showCard}>
                         { props.data.explanation }
                     </Paragraph>
+                    { props.data.copyright ? <p>{props.data.copyright}</p> : '' }
                 </InfoContainer>
             </CardContainer>
         );
